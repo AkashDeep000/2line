2cd57a909b7bf701c8da0db079bd324866d0246d
AkashDeep000 <akashdeepdas970@gmail.com>
2021-06-29 14:25:56 +0530

    fix

--- pages/status/[...slug].js
+++ pages/status/[...slug].js
@@ -11,8 +11,8 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`http://localhost:3000/api/hello`)
-  const post = await res.json()
+  
+  const post = "hello world"
 
   // Pass post data to the page via props
   return { props: { post } }
