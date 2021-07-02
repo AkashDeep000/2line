a0344cffd360765e9ef776eb4d93ff6a1acfe98d
AkashDeep000 <akashdeepdas970@gmail.com>
2021-07-02 09:47:54 +0530

    ffd

--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
   <>
    <Head>
-<script src="//cdn.jsdelivr.net/npm/eruda"></script>
-  <script src="https://cdn.jsdelivr.net/npm/eruda-dom"></script>
-<script>eruda.init();eruda.add(erudaDom);
-</script>
+
 <link rel="preconnect" href="https://cdn.statically.io/" />
 <link rel="dns-prefetch" href="https://cdn.statically.io/" />
 
